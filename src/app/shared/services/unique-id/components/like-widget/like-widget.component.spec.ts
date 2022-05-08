1f5cdb7c53d601113eec6f7f8ee0341c63ea7f86
Diego Minelli <diego.minelli@hotmail.com>
2022-05-08 15:00:44 -0300

    deteccao manual de mudancas

--- src/app/shared/services/unique-id/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/services/unique-id/components/like-widget/like-widget.component.spec.ts
@@ -1,8 +1,6 @@
 import { LikeWidgetModule } from './like-widget.module';
-import { UniqueIdService } from './../../unique-id.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
@@ -16,7 +14,21 @@ describe(LikeWidgetComponent.name, () => {
   });
 
   it('Should create component', () => {
-    const instance = fixture.componentInstance;
-    expect(instance).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT generate ID when id input property is present', () => {
+    const component = fixture.componentInstance;
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
   });
 });
