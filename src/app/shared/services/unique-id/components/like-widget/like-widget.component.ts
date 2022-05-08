7104304125a156751e008862f3f2acb4d1bcebf0
Diego Minelli <diego.minelli@hotmail.com>
2022-05-08 12:37:25 -0300

    criacao do component widget

---
+++ src/app/shared/services/unique-id/components/like-widget/like-widget.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: './like-widget.component.html',
+  styleUrls: ['./like-widget.component.scss'],
+})
+export class LikeWidgetComponent {}
