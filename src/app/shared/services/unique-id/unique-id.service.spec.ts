7d9af020bd7b45faa4fe291079332ba2a2f7112e
Diego Minelli <diego.minelli@hotmail.com>
2022-05-08 10:41:12 -0300

    estruturando o teste

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,3 +1,9 @@
-describe('O artefato que queremos testar', () => {
-  it('Primeira condiçâo que queremos testar');
+import { UniqueIdService } from './unique-id.service';
+
+describe('UniqueIdService', () => {
+  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id).toContain('app-');
+  });
 });
